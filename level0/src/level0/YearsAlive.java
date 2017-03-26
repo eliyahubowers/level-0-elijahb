9acc504684be85bb56eaadb087528b08599c75a1
eliyahubowers <league@WorthyMini-6.attlocal.net>
2017-03-26 11:32:40 -0700

    countdown

---
+++ level0/src/level0/YearsAlive.java
@@ -0,0 +1,23 @@
+package level0;
+
+public class YearsAlive {
+	public static void main(String[] args) {
+	for (int i = 0; i < 13; i++) {
+	int year=2005+i;
+	System.out.println("I was alive in "+year  );
+	
+	}	
+	for (int e = 0; e < 100; e++) {
+	int year=2018+e;
+	System.out.println("I will be alive in "+year  );
+	}
+	for (int n = 0; n < 13; n++) {
+	int year= 2017-n;	
+	System.out.println("I was alive in "+year+                                                                                                                                                                                                                                                                               																																																																																																																														                                           																																																																																																																																																																																																																																																																																																																																																							"made you look"  );
+	}
+	
+	
+	
+	
+	}	
+}
